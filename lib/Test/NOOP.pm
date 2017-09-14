608afe4e463d4d6ee745be88ed677a9709b9bda8
gabrielash <gabrielash@users.noreply.github.com>
2017-09-14 21:57:54 +0200

    Update NOOP.pm

--- lib/Test/NOOP.pm
+++ lib/Test/NOOP.pm
@@ -7,3 +7,23 @@ use v6;
 sub testme( $x is copy) is export { ++$x }
 
 my $y = 0;
+
+class zmq_msg_t is repr('CStruct') is export {
+	has int64 $._;
+	has int64 $._1;
+	has int64 $._2;
+	has int64 $._3;
+	has int64 $._4;
+	has int64 $._5;
+	has int64 $._6;
+	has int64 $._7;
+}
+
+sub zmq_msg_init_data(zmq_msg_t
+                     ,Pointer
+                     ,size_t
+                     ,Pointer = nativecast( Pointer, 0)
+                     ,Pointer = nativecast( Pointer, 0)
+                      ) is native('zmq', v5)
+                      returns int32 is export { * }
+
